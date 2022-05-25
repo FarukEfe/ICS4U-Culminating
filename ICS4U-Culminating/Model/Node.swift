51ea5a456663bcad5d1e07f8d907ef2b4c706db9
vincecloutier <vincentcloutier@mail.com>
2022-05-25 16:55:51 -0400

    added some nodes and input stuff for graph

--- ICS4U-Culminating/Model/Node.swift
+++ ICS4U-Culminating/Model/Node.swift
@@ -15,5 +15,12 @@ struct Node: Identifiable {
 }
 
 let nodes = [
-    Node(id: 1, paragraphs: ["bla bla bla", "bla bla bla bla"], image: nil, edges: [2])
+    Node(id: 1, paragraphs: [""], image: nil, edges: [7,8]), // This node will include the text from pages 1-5
+    Node(id: 7, paragraphs: [""], image: nil, edges: [9, 13]),
+    Node(id: 8, paragraphs: [""], image: nil, edges: [10]),
+    Node(id: 9, paragraphs: [""], image: nil, edges: [2]),
+    Node(id: 1, paragraphs: [""], image: nil, edges: [2]),
+
+
+    
 ]
