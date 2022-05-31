a7d26d55eaa83870cd4376e03ed642efed07b38b
vincecloutier <vincentcloutier@mail.com>
2022-05-31 14:22:15 -0400

    switched to a dictionary

---
+++ ICS4U-Culminating/Model/Edge.swift
@@ -0,0 +1,13 @@
+//
+//  Edge.swift
+//  ICS4U-Culminating
+//
+//  Created by Vincent Cloutier on 2022-05-31.
+//
+
+import Foundation
+
+struct Edge: Decodable {
+    let destinationId: Int
+    let prompt: String?
+}
