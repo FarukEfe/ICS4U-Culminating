5b352aa89b2066f0de2c94143e4b59657eef13c6
vincecloutier <vincentcloutier@mail.com>
2022-06-03 11:26:36 -0400

    completed ending explanations resolves issue #25

--- Model/EndingIDs.swift
+++ Model/EndingIDs.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-var goodEndingIDs: [Int] = [60, 47, 64, 102, 103, 104, 59, 80, 85, 99]
-var badEndingIDs: [Int] = [54, 28, 29, 33, 42, 202, 203, 204, 32, 54, 88, 96]
-var ambiguousEndingIDs: [Int] = [37, 63, 107, 111, 114, 116, 64, 72, 73, 87, 89]
+var goodEndingIDs: [Int : String] = [60 : "Discovered unicorn", 47 : "Smuggler hunter", 64 : "Undercover Sangee", 102 : "Yeti vs. Movidians", 103 : "The yeti and the fast-talker", 104 : "Pure mind-no matter.", 59 : "Atlantis crooks", 80 : "Fame is now yours", 85 : "The eleven yeti", 99 : "Life-saver"]
+var badEndingIDs: [Int : String] = [54 : "The yeti are not animals", 28 : "Tiger food", 29 : "Death by poacher", 33 : "Lifelong coma", 42 : "Rain-check", 202 : "Food-poisoning", 203 : "Coward", 204 : "A stupid, noble death", 32 : "Bronze cannon", 88 : "Death by icefall", 96 : "Waiting..."]
+var ambiguousEndingIDs: [Int : String] = [37 : "Tucked-in", 63 : "Monk memories", 107 : "Questioning", 111 : "Luddite vibes", 114 : "Failed photos", 116 : "Permit revoked", 72 : "Comfort in failure", 73 : "Discovered, with no proof", 87 : "Leave the yetis alone", 89 : "Stolen supplies"]
